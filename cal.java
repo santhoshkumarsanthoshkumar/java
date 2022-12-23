79bc35063bf88bd9d6efa196e7b8e391bfc62cf8
santhoshkumarsanthoshkumar <120361140+santhoshkumarsanthoshkumar@users.noreply.github.com>
2022-12-23 12:38:05 +0530

    change

--- cal.java
+++ cal.java
@@ -1,50 +1,7 @@
-package ca;
 import java.util.*;
-class cal
-{
-    public static void main(String []arg)
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt(),i,j;
-        boolean f=true;
-        int []a=new int[n];
-        int []b=new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt(); 
-        }
-        for(i=0;i<n;i++)
-        {
-            b[i]=a[i];
-        }
-        Arrays.sort(b);
-        for(i=0;i<n;i++)
-        {
-            if(b[i]!=a[i])
-            {
-                f=false;
-                break;
-            }
-        }
-        if(f)
-        {
-          System.out.print("Sorted");
-        }
-        if(!f)
-        {
-            for(j=0,i=n-1;i>=0;i--,j++)
-            if(b[i]!=a[j])
-            {
-                f=false;
-                break;
-            }
-            else
-            {
-                f=true;
-            }
-            System.out.print(f?"Sorted":"Unsorted");
-        }
-       
-        
-    }
+public class calcc {
+	public static void main(String []arg)
+	{
+ System.out.print("hi");
+	}
 }
